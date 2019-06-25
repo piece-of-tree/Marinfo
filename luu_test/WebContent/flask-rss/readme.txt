527dae718d2a5cb4d0f599b25231d710e34766ee
itsuki kataoka <itsuki.outtune24@gmail.com>
2019-06-25 10:49:13 +0900

    test

--- luu_test/WebContent/flask-rss/readme.txt
+++
@@ -1,6 +0,0 @@
-必要なパッケージのインストール
-$ sudo pip3 install flask flask-cors feedparser
-
-サーバーの実行
-$ export FLASK_APP=rss_api_server.py
-$ flask run --debugger --reload --host=0.0.0.0
--- luu_test/src/jp/co/comnic/lesson/yoneyama/webapp/controller/Dispatcher.java
+++
@@ -1,9 +0,0 @@
-package jp.co.comnic.lesson.yoneyama.webapp.controller;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
-public interface Dispatcher {
-    public void dispatch() throws ServletException, IOException;
-}
